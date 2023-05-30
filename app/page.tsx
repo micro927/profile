927e65aea68da81818a23863bd1949f5ea1db750
micro927 <nsbelieve-15@hotmail.com>
2023-05-30 13:56:13 +0700

    update: clock components

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,8 @@ import Clock from "./components/Clock";
 
 export default function Home() {
   return (
-    <main className="min-w-full min-h-screen flex items-center justify-between">
-      <div className="w-full flex flex-col items-center">
-        <p>Hello, Good Morning</p>
-        <Clock />
-        <div></div>
-      </div>
+    <main className="min-w-full min-h-screen flex flex-col items-center justify-center">
+      <Clock />
     </main>
   );
 }
