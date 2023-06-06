fa2638a4495b20c9b3cca725c1a7d27a82c4159f
micro927 <nsbelieve-15@hotmail.com>
2023-06-06 11:41:58 +0700

    update: for docker deploy

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@ import Clock from "./components/Clock";
 export default function Home() {
   return (
     <main className="min-w-full min-h-screen flex flex-col items-center justify-center">
-      <div className="text-6xl leading-loose font-bold text-transparent hover:text-white bg-clip-text bg-gradient-to-tr from-violet-500 from-10% to-rose-700 to-65% transition duration-300">
-        Hello Everyone
+      <div className="text-6xl text-center font-bold text-transparent hover:text-white bg-clip-text bg-gradient-to-tr from-violet-500 from-10% to-rose-700 to-65% transition duration-300">
+        Hello Everyone!
       </div>
       <div className="mt-5">
         <Clock />
+      </div>
+      <div>
+        <p className="text-xs text-zinc-700">
+          running on {process.env.PROJECT_ENV_TITLE}
+        </p>
       </div>
     </main>
   );
--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-const repo = "profile";
-const assetPrefix = isProd ? `/${repo}/` : undefined;
-const basePath = isProd ? `/${repo}` : undefined;
-
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  assetPrefix: assetPrefix,
-  basePath: basePath,
 };
 
 // const nextConfig = {};
