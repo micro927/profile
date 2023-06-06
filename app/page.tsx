e1027853450b54ea3bd63a98524023c41a77936a
micro927 <nsbelieve-15@hotmail.com>
2023-06-06 14:53:03 +0700

    add: ci/cd & docker to deploy ec2

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
+import Link from "next/link";
 import Clock from "./components/Clock";
 
 export default function Home() {
   return (
     <main className="min-w-full min-h-screen flex flex-col items-center justify-center">
       <div className="text-6xl text-center font-bold text-transparent hover:text-white bg-clip-text bg-gradient-to-tr from-violet-500 from-10% to-rose-700 to-65% transition duration-300">
-        Hello Everyone!
+        Hello Everyone
       </div>
       <div className="mt-5">
         <Clock />
       </div>
-      <div>
+      <div className="text-center">
         <p className="text-xs text-zinc-700">
-          running on {process.env.PROJECT_ENV_TITLE}
+          this website running on {process.env.PROJECT_ENV_TITLE}
+        </p>
+        <p className="text-xs text-zinc-700">
+          <Link href={"blog/15"}>Click here to see blog #15</Link>
         </p>
       </div>
     </main>
